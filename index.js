41725064c27d8cb55dcff84358d805766691f5f9
new name <rbt690916@gmail.com>
2024-03-23 01:56:43 +0800

    no message

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res, next) {
+  res.send("Hello World! ///!");
+});
+
+app.get('/about', function (req, res) {
+    res.send('about');
+  });
+
+app.get('/demo', function (req, res ) {
+res.send('demo');
+});
+
+app.listen(8000, function () {
+  console.log("API app listening on port 8000!");
+});
+
+
