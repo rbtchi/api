25877366ba291aaf9384fa08c31a03c428ec520f
RBTCHI <rbt690916@gmail.com>
2024-03-22 09:45:39 +0800

    no message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res, next) {
-  res.send("Hello World! //////!!!!,mhakjlsdhfkjdash");
+  res.send("Hello World! //////!!!!,sh");
 });
 
 app.get('/about', function (req, res) {
