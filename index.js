197bffcc8f296fcf89e70351088225936d82d7f6
new name <rbt690916@gmail.com>
2024-03-24 18:01:38 +0800

    移除lowdb

--- index.js
+++ index.js
@@ -1,24 +1,8 @@
-const express = require("express")
-const cors = require("cors")
-const lowDb = require("lowdb")
-const FileSync = require("lowdb/adapters/FileSync")
-const bodyParser = require("body-parser")
-const { nanoid } = require("nanoid")
-
-const db = lowDb(new FileSync('db.json'))
-
-db.defaults({ notes: [] }).write()
-
-const app = express()
-
-app.use(cors())
-app.use(bodyParser.json())
-
-const PORT = 8000;
+var express = require("express");
+var app = express();
 
 app.get("/", function (req, res, next) {
-  const data = db.get("notes").value()
-  return res.json(data)
+  res.send("Hello World! ///!");
 });
 
 app.get('/about', function (req, res) {
@@ -29,19 +13,8 @@ app.get('/demo', function (req, res ) {
 res.send('demo');
 });
 
-app.get('/notes', function (req, res)  {
-  const data = db.get("notes").value()
-  return res.json(data)
-})
+app.listen(8000, function () {
+  console.log("API app listening on port 8000!");
+});
 
-app.post('/notes/new',function (req, res) {
-  const note = req.body
-  db.get("notes").push({
-    ...note, id: nanoid()
-  }).write()
-  res.json({ success: true })
-})
 
-app.listen(PORT, ()=> {
-  console.log(`Backend is running on http://localhost:${PORT}`)
-})
